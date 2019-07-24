6fd7eca110026c95b3a08eeaef66961cf2de1726
Murali Veesambattu <murali.veesambattu@incedoinc.com>
2019-07-24 18:46:45 +0530

    Adding Balance JS Topics

--- sample13_form_Values.js
+++ sample13_form_Values.js
@@ -50,6 +50,7 @@ function displayUsers() {
   document.getElementById("myTable").innerHTML = "";
   for (i = 0; i < users.length; i++) {
     var myTr_ = document.createElement("tr");
+    myTr_.setAttribute("onclick","editUser("+i+")")
     for (a in users[i]) {
 
       //Capture all the values except roles
@@ -88,3 +89,33 @@ function displayUsers() {
     document.getElementById("myTable").appendChild(myTr_);
   }
 }
+
+
+function editUser(i){
+  console.log(users[i])
+  for(a in users[i]){
+    document.getElementById(a).value = users[i][a]
+  }
+}
+
+function updateUser(){
+  console.log("updateUser is called")
+}
+
+function getData(){
+  var abc = new XMLHttpRequest;
+            abc.onreadystatechange = function(){
+                if(abc.readyState == 4 && abc.status == 200){
+                    var data = JSON.parse(abc.response)
+                    console.log(data)
+                    users = data
+                    displayUsers()
+                }
+            }
+
+            abc.open("GET","http://www.filltext.com/?rows=10&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true")
+            abc.send()
+
+}
+
+getData()
