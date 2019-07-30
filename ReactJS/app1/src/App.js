84ce7a8badeffdfc68fca428a5788cb658f4643c
Murali Veesambattu <murali.veesambattu@incedoinc.com>
2019-07-30 17:50:29 +0530

    Adding Object And Array Iteration And Adding Conditional Rendering

--- ReactJS/app1/src/App.js
+++ ReactJS/app1/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Sandeep1, { Sandeep2 } from "./Sandeep";
+import Sample from "./Sample";
+// import Sandeep1, { Sandeep2 } from "./Sandeep";
 
 export default function App() {
   return (
     <div>
       <h1>Welcome to App Component</h1>
-      <Sandeep1 />
-      <Sandeep2 />
+      {/* <Sandeep1 />
+      <Sandeep2 /> */}
+      <Sample></Sample>
     </div>
   );
 }
