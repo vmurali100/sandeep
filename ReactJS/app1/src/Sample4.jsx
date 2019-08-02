cb235f32bfe4b5c5e6b5a861252a0e65e87b70f6
Murali Veesambattu <murali.veesambattu@incedoinc.com>
2019-08-02 18:49:30 +0530

    CRUD

--- ReactJS/app1/src/Sample4.jsx
+++ ReactJS/app1/src/Sample4.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import axios from "axios";
+import axios from "axios";
 
 
 export default class Sample4 extends Component {
@@ -8,13 +8,13 @@ export default class Sample4 extends Component {
     
         this.state = {
              users:[],
-             objEdit:{}
+             objEdit:obj
         }
         // this.getDataFromServer()
     }
 
     async componentDidMount(){
-        const response = await fetch(`http://www.filltext.com/?rows=5&fname={firstName}&lname={lastName}&tel={phone|format}&address={streetAddress}&city={city}&state={usState|abbr}&zip={zip}&pretty=true`);
+        const response = await fetch(`http://localhost:3000/users`);
         const json = await response.json();
         this.setState({ users: json });
     }
@@ -37,22 +37,41 @@ export default class Sample4 extends Component {
         this.setState({objEdit:obj})
     }
     deleteUser=(i)=>{
-        let delusers =this.state.users.splice(i,1)
-        console.log(delusers)
-        this.setState({users:this.state.users})
+        // let delusers =this.state.users.splice(i,1)
+        // console.log(delusers)
+        // this.setState({users:this.state.users})
         console.log("deleteUser called",i)
+        axios.delete("http://localhost:3000/users/"+this.state.users[i].id).then(res=>{
+            console.log()
+        }).catch((error)=>{
+            console.log("Error Occured")
+        })
+    }
+    handleInput(e){
+        this.state.objEdit[e.target.name] = e.target.value
+        this.setState({objEdit:this.state.objEdit})
+    }
+
+    updateUser = () =>{
+        console.log(this.state.objEdit)
+        this.state.objEdit.id=null
+        axios.post("http://localhost:3000/users/",this.state.objEdit).then(res=>{
+            console.log()
+        }).catch((error)=>{
+            console.log("Error Occured")
+        })
     }
     render() {
         return (
             <div>
-                <div class="row">
-                    <div class="col-sm-8"><table className="table">
+                <div className="row">
+                    <div className="col-sm-8"><table className="table">
                     <thead>
                         <tr>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Tel</th>
-                        <th scope="col">Address</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Tel</th>
+                        <th>Address</th>
                         <th>City</th>
                         <th>State</th>
                         <th>Zip</th>
@@ -69,22 +88,53 @@ export default class Sample4 extends Component {
                             <td><button onClick={()=>{this.deleteUser(i)}} className="btn btn-danger">Delete</button></td></tr>
                         })}
                     </tbody>
-                </table></div>
-                    <div class="col-sm-4"><form>
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                </table>
+                </div>
+                    <div className="col-sm-4"><form>
+                    <div className="form-group">
+                        <label htmlFor="fname">First Name</label>
+                        <input type="text" className="form-control" name="fname" placeholder="Enter First Name" value={this.state.objEdit.fname} onChange={(e)=>{this.handleInput(e)}}/>
                     </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <div className="form-group">
+                        <label htmlFor="lname">Last Name</label>
+                        <input type="text" className="form-control" name="lname" placeholder="Enter First Name" value={this.state.objEdit.lname} onChange={(e)=>{this.handleInput(e)}}/>
                     </div>
-                  
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    </form></div>
+                    <div className="form-group">
+                        <label htmlFor="tel">Tel</label>
+                        <input type="text" className="form-control" name="tel" placeholder="Tel" value={this.state.objEdit.tel} onChange={(e)=>{this.handleInput(e)}}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="address">Address</label>
+                        <input type="text" className="form-control" name="address" placeholder="Enter First Name" value={this.state.objEdit.address} onChange={(e)=>{this.handleInput(e)}}/>
+                    </div> 
+                    <div className="form-group">
+                        <label htmlFor="fname">City</label>
+                        <input type="text" className="form-control" name="city" placeholder="Enter First Name" value={this.state.objEdit.city} onChange={(e)=>{this.handleInput(e)}}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="state">State</label>
+                        <input type="text" className="form-control" name="state" placeholder="Enter First Name" value={this.state.objEdit.state} onChange={(e)=>{this.handleInput(e)}}/>
+                    </div> 
+                    <div className="form-group">
+                        <label htmlFor="zip">Zip</label>
+                        <input type="text" className="form-control" name="zip" placeholder="Enter First Name" value={this.state.objEdit.zip} onChange={(e)=>{this.handleInput(e)}}/>
+                    </div>    
+                    <button type="button" className="btn btn-primary" onClick={this.updateUser}>Update</button>
+                    </form>
+                </div>
                 </div>
                
             </div>
         )
     }
+}
+
+let obj={
+    fname:"",
+    lname:"",
+    tel:"",
+    address:"",
+    city:"",
+    state:"",
+    zip:''
 }
